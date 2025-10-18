b67613205ef356b9ceb401c3fc068bb87fabea4b
Diego <dpampa20990@iesjoanramis.org>
2025-10-18 20:14:08 +0200

    ve

--- pull-con-git/src/helloworld.java
+++ pull-con-git/src/helloworld.java
@@ -1,7 +1,7 @@
 public class helloworld {
 	public static void main(String[] args) {
 
-		System.out.println("Comentario en local rama2");
+		System.out.println("Comentario en local lalala");
 		System.out.println("Comentario en remoto rama master");
 	}
 }
