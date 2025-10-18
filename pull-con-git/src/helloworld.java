deea0c98b2f9c9bf24283975bbd7a8cebdcfeef1
Diego <dpampa20990@iesjoanramis.org>
2025-10-18 19:04:47 +0200

    volviendo a antes

--- pull-con-git/src/helloworld.java
+++ pull-con-git/src/helloworld.java
@@ -1,30 +1,13 @@
-import java.util.Scanner;
-
-public class Cuatro_18 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int numero;
-
-        System.out.print("Ingresa un número para convertir a binario: ");
-        numero = sc.nextInt();
-
-        int[] almacenar = new int[50];
-        int cuenta = 0;
-
-        // Guardar restos en orden inverso
-        while (numero > 0) {
-            almacenar[cuenta] = numero % 2;
-            numero /= 2;
-            cuenta++;
-        }
-
-        // Imprimir en orden inverso para mostrar binario correcto
-        System.out.print("Binario: ");
-        for (int i = cuenta - 1; i >= 0; i--) {
-            System.out.print(almacenar[i]);
-        }
-    }
+public class helloworld {
+	public static void main(String[] args) {
+		System.out.println("Hello World");	
+		System.out.println("Other World sin punto");	
+		System.out.println("Otro masss");
+		System.out.println("Hello World");	
+		System.out.println("Other World sin punto");	
+		System.out.println("Otro masss");
+		
+	}
 }
-
 
 
