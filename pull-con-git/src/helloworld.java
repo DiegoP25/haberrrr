460372012c5825cc534ddcdb82c438cde54d19db
DiegoP25 <dpampa20990@iesjoanramis.org>
2025-10-18 18:49:38 +0200

    ahora  sin los puntos

--- pull-con-git/src/helloworld.java
+++ pull-con-git/src/helloworld.java
@@ -2,7 +2,8 @@ public class helloworld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");	
 		System.out.println("Other World sin punto");	
-		System.out.println("Otro masss...");
+		System.out.println("Otro masss");
 	}
 }
+
 
