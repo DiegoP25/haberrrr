6bdc51eecc3c5a031c889172cade9ae46e2413fa
Diego <dpampa20990@iesjoanramis.org>
2025-10-18 19:28:53 +0200

    comente en el local rama2

--- pull-con-git/src/helloworld.java
+++ pull-con-git/src/helloworld.java
@@ -1,10 +1,6 @@
 public class helloworld {
 	public static void main(String[] args) {
-		System.out.println("Hello World");	
-		System.out.println("Other World sin punto");	
-		System.out.println("Otro masss");
-		System.out.println("hay vamos de nuevo");
-		System.out.println("coment en rama 2 desde github");
+		System.out.println("Comentario en local rama2");
 	}
 }
 
