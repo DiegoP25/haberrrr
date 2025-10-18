b43bb1292fa2b21092e5526c55443960ceb4e191
Diego <dpampa20990@iesjoanramis.org>
2025-10-18 19:16:16 +0200

    zzz

--- pull-con-git/src/helloworld.java
+++ pull-con-git/src/helloworld.java
@@ -6,7 +6,7 @@ public class helloworld {
 		System.out.println("Hello World");	
 		System.out.println("Other World sin punto");	
 		System.out.println("Otro masss");
-		lo he vinculadoo
+		
 	}
 }
 
