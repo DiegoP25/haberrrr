bde35bb0010e6f7d21e9dd4f87571b1ac45d0e6c
Diego <dpampa20990@iesjoanramis.org>
2025-10-18 19:27:33 +0200

    hecho en el local rama master

--- pull-con-git/src/helloworld.java
+++ pull-con-git/src/helloworld.java
@@ -1,9 +1,7 @@
 public class helloworld {
 	public static void main(String[] args) {
-		System.out.println("Hello World");	
-		System.out.println("Other World sin punto");	
-		System.out.println("Otro masss");
-		System.out.println("hay vamos de nuevo");
+		System.out.println("Comentario en local rama master");	
+		
 	}
 }
 
