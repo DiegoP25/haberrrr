d5cbe3663744c1534282222305ce83b90dee48de
DiegoP25 <dpampa20990@iesjoanramis.org>
2025-10-18 20:13:34 +0200

    suele ser  confuso

--- pull-con-git/src/helloworld.java
+++ pull-con-git/src/helloworld.java
@@ -2,9 +2,11 @@ public class helloworld {
 	public static void main(String[] args) {
 		System.out.println("Comentario en local rama master");
 		System.out.println("Comentario en remoto rama master");
+		System.out.println("suele ser confuso esto del git");
 		
 	}
 }
+
 
 
 
