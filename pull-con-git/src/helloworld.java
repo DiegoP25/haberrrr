0a8eb4219d20f998ddcc25768d55a987559c5079
Diego <dpampa20990@iesjoanramis.org>
2025-10-18 19:02:38 +0200

    Varios comentarios agregaasos

--- pull-con-git/src/helloworld.java
+++ pull-con-git/src/helloworld.java
@@ -3,6 +3,10 @@ public class helloworld {
 		System.out.println("Hello World");	
 		System.out.println("Other World sin punto");	
 		System.out.println("Otro masss");
+		System.out.println("Hello World");	
+		System.out.println("Other World sin punto");	
+		System.out.println("Otro masss");
+		
 	}
 }
 
