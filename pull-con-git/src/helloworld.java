9e041ab2525cb441c6c185d9545a2a60ff82b9c5
Diego <dpampa20990@iesjoanramis.org>
2025-10-18 19:21:29 +0200

    vamos de nuevo

--- pull-con-git/src/helloworld.java
+++ pull-con-git/src/helloworld.java
@@ -3,7 +3,7 @@ public class helloworld {
 		System.out.println("Hello World");	
 		System.out.println("Other World sin punto");	
 		System.out.println("Otro masss");
-	
+		System.out.println("hay vamos de nuevo");
 	}
 }
 
