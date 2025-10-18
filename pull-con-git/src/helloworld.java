e7a88af4941baa19fb87b09c89da04d266b810ff
DiegoP25 <dpampa20990@iesjoanramis.org>
2025-10-18 19:28:12 +0200

    coment made from remote

--- pull-con-git/src/helloworld.java
+++ pull-con-git/src/helloworld.java
@@ -1,9 +1,11 @@
 public class helloworld {
 	public static void main(String[] args) {
-		System.out.println("Comentario en local rama master");	
+		System.out.println("Comentario en local rama master");
+		System.out.println("Comentario en remoto rama master");
 		
 	}
 }
+
 
 
 
